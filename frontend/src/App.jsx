f68d33ce7a275f63b0802586d27b5a8e8cfac337
pramudi27 <pramukarunarathna27@gmail.com>
2025-12-16 07:02:24 +0530

    Update frontend components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
-  return <div className="text-red-500">App</div>;
-}
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import PageLayout from "./components/layout/PageLayout";
+import UserManagementPage from "./features/management/pages/UserManagementPage";
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PageLayout />}>
+          <Route index element={<Navigate to="/employees" replace />} />
+          <Route path="/employees" element={<UserManagementPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}

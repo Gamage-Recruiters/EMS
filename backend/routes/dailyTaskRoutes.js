b325e56e42e79bd4624061ac63f98490b109645e
ASDENUWAN <sachinthaakarawita@gmail.com>
2025-12-19 14:14:11 +0530

    added getAllDailyTasks,updatePMCheck,updateTLCheck

--- backend/routes/dailyTaskRoutes.js
+++ backend/routes/dailyTaskRoutes.js
@@ -2,6 +2,9 @@ import express from "express";
 import {
   createDailyTask,
   getMyDailyTasks,
+  getAllDailyTasks,
+  updatePMCheck,
+  updateTLCheck,
 } from "../controllers/dailyTaskController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
@@ -11,5 +14,10 @@ const router = express.Router();
 router.post("/", protect, createDailyTask);
 // Developer gets their task list (full details)
 router.get("/my", protect, getMyDailyTasks);
+
+// PM / TL / CEO
+router.get("/", protect, getAllDailyTasks);
+router.put("/:id/pm-check", protect, updatePMCheck);
+router.put("/:id/tl-check", protect, updateTLCheck);
 
 export default router;

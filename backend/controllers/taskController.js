3dfefa186c052e8d599ea649684d7c86059fe3cb
Steph-kushi12 <kushistephanie@gmail.com>
2025-12-22 10:59:26 +0530

    Created taskController.js

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,37 @@
+const Task = require("../models/Task");
+
+exports.createTask = async (req, res) => {
+  try {
+    const {
+      title,
+      description,
+      assignedTo,
+      dueDate,
+      priority,
+    } = req.body;
+
+    // assignedBy comes from logged in user
+    const assignedBy = req.user.id;
+
+    const task = await Task.create({
+      title,
+      description,
+      assignedBy,
+      assignedTo,
+      dueDate,
+      priority,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Task assigned successfully",
+      data: task,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to assign task",
+      error: error.message,
+    });
+  }
+};

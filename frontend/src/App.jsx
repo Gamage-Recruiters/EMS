ddc9417cffc1895e037916faedddce0590a8cda2
IT22026170chathuri <135439160+IT22026170chathuri@users.noreply.github.com>
2025-12-23 10:02:47 +0530

    add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
+import Register from './pages/auth/Register.jsx';
+import Login from './pages/auth/Login.jsx';
+import ForgotPassword from "./pages/auth/ForgotPassword.jsx";
+import VerifyCode from "./pages/auth/VerifyCode.jsx";
+import ResetPassword from "./pages/auth/ResetPassword.jsx";
+import ResetSuccess from "./pages/auth/ResetSuccess.jsx";
+
 // Profile Pages
 import PersonalDetailsPage from "./pages/profile/PersonalDetails.jsx";
 import ContactDetailsPage from "./pages/profile/ContactDetails.jsx";
@@ -9,6 +16,10 @@ import JobDetailsPage from "./pages/profile/JobDetails.jsx";
 
 // Dashboard
 import DashboardOverview from "./pages/dashboard/DashBoardOverview.jsx";
+import CeoDashboard from "./pages/dashboard/CeoDashboard.jsx";
+import DevDashboard from "./pages/dashboard/DevDashboard.jsx";
+import SystemOwnerDashboard from "./pages/dashboard/SystemOwnerDashboard.jsx";
+import TLDashboard from "./pages/dashboard/TLDashboard.jsx";
 
 // Components
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
@@ -16,8 +27,17 @@ import ProtectedRoute from "./components/ProtectedRoute.jsx";
 function App() {
   return (
     <Routes>
-      {/* Default route */}
-      <Route path="/" element={<Navigate to="/profile/personal" replace />} />
+      {/* Default route - redirect to login */}
+      <Route path="/" element={<Navigate to="/login" replace />} />
+
+       {/* Auth pages route */}
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/verify-code" element={<VerifyCode />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/reset-success" element={<ResetSuccess />} />
 
       {/* Profile page routes */}
       <Route
@@ -53,8 +73,6 @@ function App() {
         }
       />
 
-      
-
       {/* Redirect /profile -> /profile/personal */}
       <Route path="/profile" element={<Navigate to="/profile/personal" replace />} />
 
@@ -64,6 +82,43 @@ function App() {
         element={
           <ProtectedRoute>
             <DashboardOverview />
+          </ProtectedRoute>
+        }
+      />
+
+        {/* RBAC dashboards */}
+      <Route
+        path="/dashboard/ceo"
+        element={
+          <ProtectedRoute allowedRoles={["CEO"]}>
+            <CeoDashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/dashboard/dev"
+        element={
+          <ProtectedRoute allowedRoles={["DEVELOPER"]}>
+            <DevDashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/dashboard/system-owner"
+        element={
+          <ProtectedRoute allowedRoles={["SYSTEM_OWNER"]}>
+            <SystemOwnerDashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/dashboard/tl"
+        element={
+          <ProtectedRoute allowedRoles={["TL"]}>
+            <TLDashboard />
           </ProtectedRoute>
         }
       />
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
+import { AuthProvider } from './context/AuthContext.jsx'
 import App from './App.jsx'
+
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
+    <GoogleOAuthProvider clientId={clientId}>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </GoogleOAuthProvider>
   </StrictMode>,
 )

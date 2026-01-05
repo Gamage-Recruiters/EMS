895843f103da673f037dd7543e0a421b68c2915c
ASDENUWAN <sachinthaakarawita@gmail.com>
2026-01-05 16:55:31 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,18 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 
-import authRoutes from './routes/authRoutes.js';
-import taskRoutes from './routes/taskRoutes.js';
-// import testRoutes from './routes/testRoutes.js';
-import errorHandler from './middlewares/errorMiddleware.js';
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import teamRoutes from "./routes/teamRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+
+import errorHandler from "./middlewares/errorMiddleware.js";
+import leaveRoutes from "./routes/leaveRoutes.js";
+import AttendanceRoutes from "./routes/AttendanceRoutes.js";
+import availabilityRoutes from "./routes/availabilityRoutes.js";
 
 dotenv.config();
 
@@ -38,11 +43,16 @@ app.get("/", (req, res) => {
 });
 
 // Serve uploaded images statically
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/tasks', taskRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/team", teamRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/attendance", AttendanceRoutes);
+app.use("/api/leaves", leaveRoutes);
+app.use("/api/availability", availabilityRoutes);
 // app.use('/test', testRoutes);
 
 // Global Error Handler

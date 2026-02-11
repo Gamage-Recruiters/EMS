2952cc5181edc9917a5c33a6dad3955f892c40a4
Pratheesha Samadhi <pratheeshasilva26@gmail.com>
2026-02-11 20:45:44 +0530

    fix/added the missing routes in the backend to the frontend taskservice file

--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -14,4 +14,17 @@ export const taskService = {
   // Get all tasks for Kanban board
   allTasks: () => api.get("/tasks"),
 
+  // Get all users (for developer dropdown)
+  getAllUsers: () => api.get("/tasks/users"),
+
+  getAllProjects: () => api.get("/tasks/projects"),
+
+  // Update task details (edit form)
+  updateTask: (id, payload) => 
+    api.put(`/tasks/${id}`, payload),
+
+  // Delete task
+  deleteTask: (id) => 
+    api.delete(`/tasks/${id}`),
+
 };

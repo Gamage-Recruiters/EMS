0cc935ea9ff814fbdfbc602678f6c8f7f5a32b19
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-04-28 13:52:21 +0530

    fix: improve error handling in error middleware by conditionally logging errors(BUG-002: Security Leak via Stack Traces) 😎

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -1,9 +1,11 @@
 const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
+  if (process.env.NODE_ENV !== 'test') {
+    console.error(err);
+  }
   res.status(statusCode).json({
     status: err.status || 'error',
     message: err.message || 'Server Error',
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   });
 };
 
